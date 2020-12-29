8642aee08b6b5e8b407317ca2bdc9995aae81de8
75797937 <leo.mery@efrei.net>
2020-12-29 04:16:34 +0100

    css concept login page

--- client/components/Login.vue
+++ client/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
     
-    <div>
+    <div class="login-box">
         <h2>Login</h2>
         <!--<form @submit.prevent="loginUser()">
             <input v-model="pseudo" placeholder="pseudo">
@@ -12,29 +12,26 @@
     </form>-->
     <!--<link rel="stylesheet" type="text/css" href="css/LoginAndRegister.css">-->
     <form  @submit.prevent="loginUser() ">
-        <span class="register-form-title">
-            Joignez Vous Aux Quiz !
-        </span>
-
-        <div class="registerInfo validate-input" >
-            <input v-model="pseudo" class="inputinfo" type="text"  placeholder="Entrer votre pseudo" required>
+        <div class="registerInfo validate-input user-box" >
+            <input v-model="pseudo" class="inputinfo" type="text" required>
+            <label>Pseudo</label>
         </div>
 
-        <div class="registerInfo validate-input" >
-            <input v-model="password" class="inputinfo" type="password"  placeholder="Password" required>
+        <div class="registerInfo validate-input user-box" >
+            <input v-model="password" class="inputinfo" type="password" required>
+            <label>Mot de Passe</label>
         </div>
         
         <div class="container-login100-form-btn">
             <!--<router-link to='/account' class="login100-form-btn">Login</router-link>-->
             <button class="login100-form-btn">
-                Login
+                Let's GO !
             </button>
-        </div>
     </form>
     
 
-    <div class="text-center p-t-136">
-        <router-link to='/register' class="login100-form-btn">Register Now</router-link>
+    <div class="text-center p-t-136 registerLink">
+        <router-link to='/register' class="login100-form-btn">No account ? Register Now !</router-link>
         <!--<a class="txt2" href="#">
             Register Now !
             <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
