1c5c8c89deb8e33639d61bc58bccf127a2764649
lucas <lucas.gerard@efrei.net>
2021-01-05 18:54:13 +0100

    CCS For Home.Vue

--- client/components/Home.vue
+++ client/components/Home.vue
@@ -1,14 +1,14 @@
 <template>
-    
-    <div>
-        <h2>WELCOME to the Oh No Oh No Oh Nononono quizz</h2>
-        <nav>  
-            <router-link to='/login' class="login">Log in</router-link>
-            <router-link to='/register' class="login">Register</router-link>
-        </nav>  
-    </div>
+    <center>
+        <div>
+            <h2>WELCOME to the Oh No Oh No Oh Nononono quizz</h2>
+            <nav>  
+                <router-link to='/login' class="login">Log in</router-link>
+                <router-link to='/register' class="login">Register</router-link>
+            </nav>  
+        </div>
+    <center>
 </template>
-
 
 <style>
       @import '../CSS/index.css';
