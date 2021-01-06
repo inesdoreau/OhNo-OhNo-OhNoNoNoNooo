1e7f4ae4b9aea6564cd231653aaf7bbc8c7e5074
Leomry <leo.mery@efrei.net>
2021-01-06 19:20:23 +0100

    home + header

--- client/components/Home.vue
+++ client/components/Home.vue
@@ -1,12 +1,20 @@
 <template>
     <center>
-        <div>
-            <h2>WELCOME to the Oh No Oh No Oh Nononono quizz</h2>
-            <nav>  
-                <router-link to='/login' class="login">Log in</router-link>
-                <router-link to='/register' class="login">Register</router-link>
-            </nav>  
+        <div class="div-home">
+            <h2 class="home-title">WELCOME to the Oh No Oh No Oh Nononono quizz</h2>   
+        </div>  
+        <div id="wrapper">
+            <router-link to='/register' class="my-super-cool-btn">
+                <div class="dots-container">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                </div>
+                <span>Go!</span>
+            </router-link>
         </div>
+
     <center>
 </template>
 
